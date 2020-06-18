6b7735d87d4996157206b5ce38f3d366f464ddfe
sharifahmad <sharifahmad2061@gmail.com>
2020-06-18 22:28:14 +0500

    implements basic search functionality

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from .models import City
 # Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'cities/home.html'
+
+
+class SearchResultsView(ListView):
+    model = City
+    template_name = 'cities/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = City.objects.filter(
+            Q(name__icontains=query) |
+            Q(state__icontains=query)
+        )
+        return object_list
