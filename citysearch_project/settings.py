060d906e7c7983db3ff6ca538adcdba7738e9857
sharifahmad <sharifahmad2061@gmail.com>
2020-06-18 21:41:18 +0500

    added a custom user model for the project which inherits from AbstractUser

--- citysearch_project/settings.py
+++ citysearch_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cities.apps.CitiesConfig'
+    'users.apps.UsersConfig',
+    'cities.apps.CitiesConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# custom user model
+AUTH_USER_MODEL = 'users.CustomUser'
